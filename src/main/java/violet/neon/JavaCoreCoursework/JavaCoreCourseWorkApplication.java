460efdd88fffa4009de4dfcbe32b319bbe087e68
Куликов Валерий Романович <VRKulikov@beeline.ru>
2025-01-26 08:53:28 +0300

    Initial commit

---
+++ src/main/java/violet/neon/JavaCoreCoursework/JavaCoreCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package violet.neon.JavaCoreCoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoreCourseWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/violet/neon/JavaCoreCoursework/JavaCoreCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package violet.neon.JavaCoreCoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoreCourseWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
